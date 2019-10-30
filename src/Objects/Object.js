3515e582ddef33958ccf61eda68cd3c21ad05b10
Joshua Starling <joshstarling97@gmail.com>
2019-10-30 18:16:22 -0400

    Added Tasks in Objects file

---
+++ src/Objects/Object.js
@@ -0,0 +1,17 @@
+const TodoObjects = [
+    {
+    task: "Work",
+    id: 1,
+    completed: false
+    },
+    {
+        task: "Get Rich",
+        id: 2,
+        completed: false
+    },
+    {
+        task: "Retire",
+        id: 3,
+        completed: false
+    }
+]
